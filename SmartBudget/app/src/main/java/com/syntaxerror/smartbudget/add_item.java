af81c96aa96d76215fd2d6208ee9f5d37e1dd727
minoltan <minoltan_se15021@kln.ac.lk>
2018-09-14 18:06:58 +0530

    Dropdown box re-edited

--- SmartBudget/app/src/main/java/com/syntaxerror/smartbudget/add_item.java
+++ SmartBudget/app/src/main/java/com/syntaxerror/smartbudget/add_item.java
@@ -2,16 +2,60 @@ package com.syntaxerror.smartbudget;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by minoltan on 9/14/2018.
  */
 
 public class add_item extends AppCompatActivity {
+
+    EditText amount,description;
+    Button add, cancel;
+    DBManager MyDB;
+    
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_item);
+        MyDB = new DBManager(this);
+        
+        amount = (EditText) findViewById(R.id.name);
+        description  = (EditText) findViewById(R.id.name);
+        add = (Button) findViewById(R.id.addbutton);
+        cancel = (Button) findViewById(R.id.cancelbutton);
+
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+        
+    }
+    
+
+    public void showMessage (String title , String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
