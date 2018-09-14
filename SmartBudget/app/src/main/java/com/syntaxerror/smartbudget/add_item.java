f3cf53f968347c645b8f0e52533a4843590dabe5
minoltan <minoltan_se15021@kln.ac.lk>
2018-09-14 15:39:34 +0530

    Add item added

---
+++ SmartBudget/app/src/main/java/com/syntaxerror/smartbudget/add_item.java
@@ -0,0 +1,17 @@
+package com.syntaxerror.smartbudget;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by minoltan on 9/14/2018.
+ */
+
+public class add_item extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item);
+    }
+}
